5360cf237c4ca36271817c04187cb443842bef8e
Mohd Uvais <mohduvais591@gmail.com>
2026-08-05 22:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (40.36%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidates = 0;
+        int count = 0;
+        for(int num : nums){
+            if(count==0){
+                candidates = num;
+            }
+            if(num == candidates){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidates;
+
+        
+    }
+};
